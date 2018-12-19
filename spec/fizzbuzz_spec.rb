7641efb1aa7040cd0cb9fd190cdae4ec263ed8b4
Erin Stevenson <estevenson_94@live.com>
2018-12-19 12:35:30 +0000

    new spec method

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,15 +1,15 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(3.fizzbuzz).to eq 'fizz'
   end
   it 'returns "fizz" when passed 9' do
-    expect(fizzbuzz(9)).to eq 'fizz'
+    expect(9.fizzbuzz).to eq 'fizz'
   end
   it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(5.fizzbuzz).to eq 'buzz'
   end
   it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
   end
 end
